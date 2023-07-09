c069f615274f10687da364965e92a5eacc8fbc37
Piyush76 <piyush1071singh@gmail.com>
2023-07-09 12:43:07 +0530

    monster city

--- app/src/main/java/com/devdroid/flying_fish/MainActivity5.java
+++ app/src/main/java/com/devdroid/flying_fish/MainActivity5.java
@@ -1,15 +1,33 @@
 package com.devdroid.flying_fish;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity5 extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+
 
     Button btneasy,btnmedium,btnhard;
     @SuppressLint("MissingInflatedId")
@@ -17,6 +35,12 @@ public class MainActivity5 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+
+        recyclerView=findViewById(R.id.recyclerview);
+
+        listingdata();
+        LinearLayoutManager layoutManager= new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        recyclerView.setLayoutManager(layoutManager);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
@@ -46,5 +70,67 @@ public class MainActivity5 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    private void listingdata(){
+        ApiInterface apiInterface=Retrofit.getRetrofit().create(ApiInterface.class);
+        Call<List<Pojo>> listingdata=apiInterface.getposts();
+        listingdata.enqueue(new Callback<List<Pojo>>() {
+            @Override
+            public void onResponse(Call<List<Pojo>> call, Response<List<Pojo>> response) {
+                if (response.isSuccessful()){
+                    recycleradapter adapter=new recycleradapter(response.body());
+                    recyclerView.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Pojo>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "failure",Toast.LENGTH_SHORT);
+
+            }
+        });
+    }
+
+    class recycleradapter extends RecyclerView.Adapter<recycleradapter.MyViewHolder>{
+
+        List<Pojo> list;
+
+        public recycleradapter(List<Pojo> list) {
+            this.list = list;
+        }
+
+        @NonNull
+        @Override
+        public recycleradapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            // inflate the layout here
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.sample_recyclerview, parent ,false);
+            recycleradapter.MyViewHolder viewHolder=new recycleradapter.MyViewHolder(view);
+            //used parent with false so that all items are of same size
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull recycleradapter.MyViewHolder holder, int position) {
+            holder.name.setText(list.get(position).getName());
+            holder.detail.setText(list.get(position).getUsername());
+        }
+
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+        class MyViewHolder extends RecyclerView.ViewHolder{
+
+            TextView name,detail;
+            ImageView imageView;
+
+            public MyViewHolder(@NonNull View itemView) {
+                super(itemView);
+
+                name=itemView.findViewById(R.id.Name);
+                detail=itemView.findViewById(R.id.detail);
+                imageView=itemView.findViewById(R.id.imageView);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/devdroid/flying_fish/Pojo.java
@@ -0,0 +1,148 @@
+
+
+package com.devdroid.flying_fish;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
+
+
+
+
+
+
+
+public class Pojo {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("username")
+    @Expose
+    private String username;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("address")
+    @Expose
+    private Address address;
+    @SerializedName("phone")
+    @Expose
+    private String phone;
+    @SerializedName("website")
+    @Expose
+    private String website;
+    @SerializedName("company")
+    @Expose
+    private Company company;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Pojo withId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Pojo withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Pojo withUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Pojo withEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Pojo withAddress(Address address) {
+        this.address = address;
+        return this;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Pojo withPhone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public Pojo withWebsite(String website) {
+        this.website = website;
+        return this;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Pojo withCompany(Company company) {
+        this.company = company;
+        return this;
+    }
+
+}
